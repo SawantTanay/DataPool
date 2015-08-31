536c900fa02a46d3eab7b86ab47b9b535e7d2ba5
Intelliswift <intelliswift@localhost.localdomain>
2015-08-31 17:28:36 +0530

    Added NamedQuery

--- src/com/intelliswift/beans/RetailerConfigs.java
+++ src/com/intelliswift/beans/RetailerConfigs.java
@@ -7,16 +7,19 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.Cascade;
 
 @Entity
+@NamedQuery(name="RetailerConfigs",query="from RetailerConfigs")
 public class RetailerConfigs
 {
-	@Id
-	@Column (name="Site_Id")
+    @Id
+    @Column (name="Site_Id")
     private String site_id;
     @Column (name="Site")
     private String siteName;
@@ -32,50 +35,50 @@ public class RetailerConfigs
     @OneToOne  (mappedBy="retailerConfigs")
     @Cascade(value=org.hibernate.annotations.CascadeType.SAVE_UPDATE)
     private SiteLog siteLog = new SiteLog();
-	
-    
-	public SiteLog getSiteLog() {
-		return siteLog;
-	}
-	public void setSiteLog(SiteLog siteLog) {
-		this.siteLog = siteLog;
-	}
-	public String getSite_id() {
-		return site_id;
-	}
-	public void setSite_id(String site_id) {
-		this.site_id = site_id;
-	}
-	public String getSiteName() {
-		return siteName;
-	}
-	public void setSiteName(String siteName) {
-		this.siteName = siteName;
-	}
-	public String getRetailer() {
-		return retailer;
-	}
-	public void setRetailer(String retailer) {
-		this.retailer = retailer;
-	}
-	public String getLocale() {
-		return locale;
-	}
-	public void setLocale(String locale) {
-		this.locale = locale;
-	}
-	public String getDomain() {
-		return domain;
-	}
-	public void setDomain(String domain) {
-		this.domain = domain;
-	}
-	public List<Products> getProductList() {
-		return productList;
-	}
-	public void setProductList(List<Products> productList) {
-		this.productList = productList;
-	}
-    
-    
+
+
+    public SiteLog getSiteLog() {
+        return siteLog;
+    }
+    public void setSiteLog(SiteLog siteLog) {
+        this.siteLog = siteLog;
+    }
+    public String getSite_id() {
+        return site_id;
+    }
+    public void setSite_id(String site_id) {
+        this.site_id = site_id;
+    }
+    public String getSiteName() {
+        return siteName;
+    }
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+    public String getRetailer() {
+        return retailer;
+    }
+    public void setRetailer(String retailer) {
+        this.retailer = retailer;
+    }
+    public String getLocale() {
+        return locale;
+    }
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+    public String getDomain() {
+        return domain;
+    }
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    public List<Products> getProductList() {
+        return productList;
+    }
+    public void setProductList(List<Products> productList) {
+        this.productList = productList;
+    }
+
+
 }
